28b5520956996a7d96b49b77c72077539ab2b252
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-10-08 16:33:52 -0500

    Finished radioClicked method.

--- app/src/main/java/com/example/distanceconverter/MainActivity.java
+++ app/src/main/java/com/example/distanceconverter/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.distanceconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.distanceconverter.R.id;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void radioClicked(View v) {
+        TextView inputLabel = (TextView)findViewById(id.inputLabel);
+        TextView outputLabel = (TextView)findViewById(id.outputLabel);
+        switch (v.getId()) {
+            case R.id.radioButtonMilesToKilometers:
+                inputLabel.setText("Miles Value:");
+                outputLabel.setText("Kilometers Value:");
+                break;
+            case R.id.radioButtonKilometersToMiles:
+                inputLabel.setText("Kilometers Value:");
+                outputLabel.setText("Miles Value:");
+                break;
+        }
     }
 }
