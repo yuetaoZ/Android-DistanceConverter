5f9ef8529bb5dcf62e33e0122556d91e2ebf9c9a
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-10-10 17:39:20 -0500

    Finished convertButtonClicked() method. Fixed some warning about Strings.

--- app/src/main/java/com/example/distanceconverter/MainActivity.java
+++ app/src/main/java/com/example/distanceconverter/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.distanceconverter.R.id;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+    private boolean convertFromMilesToKilometers = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +27,24 @@ public class MainActivity extends AppCompatActivity {
             case R.id.radioButtonMilesToKilometers:
                 inputLabel.setText("Miles Value:");
                 outputLabel.setText("Kilometers Value:");
+                convertFromMilesToKilometers = true;
                 break;
             case R.id.radioButtonKilometersToMiles:
                 inputLabel.setText("Kilometers Value:");
                 outputLabel.setText("Miles Value:");
+                convertFromMilesToKilometers = false;
                 break;
+        }
+    }
+
+    public void convertButtonClicked(View v) {
+        EditText inputValue = findViewById(id.inputValue);
+        TextView outputValue = findViewById(id.outputValue);
+        if (!inputValue.getText().toString().isEmpty()) {
+            String text = inputValue.getText().toString();
+            double inputNumber = Double.parseDouble(text);
+            double output = convertFromMilesToKilometers == true ? (inputNumber * 1.60934) : (inputNumber * 0.621371);
+            outputValue.setText(new DecimalFormat("##.#").format(output));
         }
     }
 }
