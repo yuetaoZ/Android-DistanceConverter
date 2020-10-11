8e994076678a33ec7e8ccf65355f456c34e2b6a2
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-10-10 19:38:56 -0500

    Update convertButtonClicked() method for conversion history. Finished updateConvertHistory() method.

--- app/src/main/java/com/example/distanceconverter/MainActivity.java
+++ app/src/main/java/com/example/distanceconverter/MainActivity.java
@@ -38,13 +38,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void convertButtonClicked(View v) {
-        EditText inputValue = findViewById(id.inputValue);
-        TextView outputValue = findViewById(id.outputValue);
-        if (!inputValue.getText().toString().isEmpty()) {
-            String text = inputValue.getText().toString();
+        EditText inputValueSlot = findViewById(id.inputValue);
+        TextView outputValueSlot = findViewById(id.outputValue);
+
+        if (!inputValueSlot.getText().toString().isEmpty()) {
+            String text = inputValueSlot.getText().toString();
             double inputNumber = Double.parseDouble(text);
-            double output = convertFromMilesToKilometers == true ? (inputNumber * 1.60934) : (inputNumber * 0.621371);
-            outputValue.setText(new DecimalFormat("##.#").format(output));
+            double outputNumber = convertFromMilesToKilometers == true ? (inputNumber * 1.60934) : (inputNumber * 0.621371);
+            outputValueSlot.setText(new DecimalFormat("##.#").format(outputNumber));
+
+            updateConvertHistory(inputNumber, outputNumber);
         }
     }
+
+    private void updateConvertHistory(double inputNumber, double outputNumber) {
+        TextView convertHistorySlot = findViewById(id.conversionHistoryRecord);
+        String convertHistory = convertHistorySlot.getText().toString();
+        String inputType, outputType;
+        inputType = convertFromMilesToKilometers == true ? " Mi" : " Km";
+        outputType = convertFromMilesToKilometers == true ? " Km" : " Mi";
+
+        convertHistorySlot.setText(new DecimalFormat("##.#").format(inputNumber) + inputType + " ==> "
+                                    + new DecimalFormat("##.#").format(outputNumber) + outputType + "\n"
+                                    + convertHistory);
+    }
+
+
 }
